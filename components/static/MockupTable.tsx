c35acfcf460b6f50e8bb9d4c750ea0db22f008c7
Chinnatip Taemkaeo <chinnatip@me.com>
2020-07-16 04:12:01 +0700

    build and deploy

--- components/static/MockupTable.tsx
+++ components/static/MockupTable.tsx
@@ -1,13 +1,13 @@
 export const tableHead = ['Name', 'Surname', 'Phone', 'Email']
 
 export const tableRows = [
-  ['Liam', 'Smith', 62223222662, 'jonsmith@mail.com'],
-  ['Liam', 'Smith', 62223222662, 'jonsmith@mail.com'],
-  ['Liam', 'Smith', 62223222662, 'jonsmith@mail.com'],
-  ['Liam', 'Smith', 62223222662, 'jonsmith@mail.com'],
-  ['Liam', 'Smith', 62223222662, 'jonsmith@mail.com'],
-  ['Liam', 'Smith', 62223222662, 'jonsmith@mail.com'],
-  ['Liam', 'Smith', 62223222662, 'jonsmith@mail.com'],
-  ['Liam', 'Smith', 62223222662, 'jonsmith@mail.com'],
-  ['Liam', 'Smith', 62223222662, 'jonsmith@mail.com'],
+  ['Carson', 'Smith', 62223222662, 'jonsmith@mail.com'],
+  ['Carson', 'Smith', 62223222662, 'jonsmith@mail.com'],
+  ['Carson', 'Smith', 62223222662, 'jonsmith@mail.com'],
+  ['Carson', 'Smith', 62223222662, 'jonsmith@mail.com'],
+  ['Carson', 'Smith', 62223222662, 'jonsmith@mail.com'],
+  ['Carson', 'Smith', 62223222662, 'jonsmith@mail.com'],
+  ['Carson', 'Smith', 62223222662, 'jonsmith@mail.com'],
+  ['Carson', 'Smith', 62223222662, 'jonsmith@mail.com'],
+  ['Carson', 'Smith', 62223222662, 'jonsmith@mail.com'],
 ]
