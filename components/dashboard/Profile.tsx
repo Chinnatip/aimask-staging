97e70c74583ae43edf27b3c483e7533830b3898e
Chinnatip Taemkaeo <chinnatip@me.com>
2020-07-17 17:15:34 +0700

    add image-gallery

--- components/dashboard/Profile.tsx
+++ components/dashboard/Profile.tsx
@@ -1,7 +1,9 @@
 import Player from '../stuff/Player'
 import { useProfile } from '../../store/index'
+import { photos } from '../static/index'
 import SocialMeter from '../stuff/SocialMeter'
 import ProfileCard from '../stuff/ProfileCard'
+import Gallery from 'react-photo-gallery'
 
 const Profile = () => {
   const {
@@ -17,6 +19,10 @@ const Profile = () => {
       <div className="grid grid-rows-3 grid-flow-col gap-4">
         <div className="row-span-1 col-span-2 ">
           <Player link={feature_video} width="480px" />
+          <br />
+          <div style={{ width: '480px' }}>
+            <Gallery photos={photos} />
+          </div>
         </div>
         <div className="row-span-3 col-span-2 ">
           <h3 className="text-4xl font-normal leading-normal mt-0 mb-2 text-indigo-700">
--- components/static/index.tsx
+++ components/static/index.tsx
@@ -1,1 +1,2 @@
 export { profileData } from './ProfileData'
+export { photos } from './Photos'
