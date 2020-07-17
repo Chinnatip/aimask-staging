f3fbd0f8150c6c15af3d0e7a628849398f88d9e0
Chinnatip Taemkaeo <chinnatip@me.com>
2020-07-17 13:38:38 +0700

    config fontawesome icon 5.1.4

--- components/static/LayoutAdminData.tsx
+++ components/static/LayoutAdminData.tsx
@@ -1,24 +1,30 @@
 import {
-  faPlus,
-  faPaperclip,
-  faSuitcase,
+  faGlobe,
+  faIdCard,
+  faCoins,
+  faHandHoldingHeart,
 } from '@fortawesome/free-solid-svg-icons'
 
 export const navList = [
   {
     path: 'dashboard',
     text: 'Overview',
-    fa_icon: faPlus,
+    fa_icon: faGlobe,
   },
   {
     path: 'finance',
     text: 'Finance',
-    fa_icon: faPaperclip,
+    fa_icon: faCoins,
+  },
+  {
+    path: 'profile',
+    text: 'Profile',
+    fa_icon: faIdCard,
   },
   {
     path: 'support',
     text: 'Support',
-    fa_icon: faSuitcase,
+    fa_icon: faHandHoldingHeart,
   },
 ]
 
@@ -26,6 +32,10 @@ export const hoverList = [
   {
     text: 'Finance',
     path: 'finance',
+  },
+  {
+    text: 'Profile',
+    path: 'profile',
   },
   {
     text: 'Support',
