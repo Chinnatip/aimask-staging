da102df0c203e0794314bcecdb4ff505b8694d02
Chinnatip Taemkaeo <chin@kohlife.com>
2021-01-24 12:11:09 +0700

    add notifier bar#2

--- pages/secret.tsx
+++ pages/secret.tsx
@@ -2,6 +2,12 @@ const Page = () => {
   return <div>
     <p className="h-10">yoyo</p>
     <p className="h-6">yayaaa</p>
+    <div className="bg-orange-800" />
+    <div className=" bg-blue-600" />
+    <div className=" bg-pink-600" />
+    <div className=" bg-teal-700" />
+    <div className=" bg-purple-700" />
+    <div className="gray-600" />
   </div>
 }
 
