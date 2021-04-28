fb95ed45384d8909c37bf8e8979bf98257faac8f
Chinnatip Taemkaeo <chin@kohlife.com>
2021-04-28 17:43:59 +0700

    api #2

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   if (req.method === 'POST') {
     // Process a POST request
-    res.status(200).json(req)
+    res.status(200).json({ response: req })
   } else {
     // Handle any other HTTP method
     res.status(200).json({ name: 'sorry other method is disable for now' })
